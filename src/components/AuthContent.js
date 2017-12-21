abad2736d957342ffce22b7ea094d71711633f76
KON3427 <aubrey.sarna@kroger.com>
2017-12-21 16:57:47 -0500

    add cards

--- src/components/AuthContent.js
+++ src/components/AuthContent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import fire from './../fire.js';
+import { Card, Image, Icon } from 'semantic-ui-react';
 
 export default class authContent extends Component {
   constructor(props) {
@@ -11,7 +12,7 @@ export default class authContent extends Component {
 
   componentWillMount() {
     /* Create reference to items in Firebase Database */
-    let itemsRef = fire.database().ref(`items/${this.props.user.uid}`).orderByKey().limitToLast(100);
+    let itemsRef = fire.database().ref(`${this.props.user.uid}/items`).orderByKey().limitToLast(100);
     itemsRef.on('child_added', snapshot => {
       /* Update React state when item is added at Firebase Database */
       let item = { text: snapshot.val(), id: snapshot.key };
@@ -22,7 +23,7 @@ export default class authContent extends Component {
   addItem(e){
     e.preventDefault();
     /* Send the message to Firebase */
-    fire.database().ref(`items/${this.props.user.uid}`).push(this.inputEl.value);
+    fire.database().ref(`${this.props.user.uid}/items`).push(this.inputEl.value);
     this.inputEl.value = ''; // <- clear the input
   }
 
@@ -30,15 +31,42 @@ export default class authContent extends Component {
     return (
       <div>
         <h1>My stuff</h1>
-        <form onSubmit={this.addItem.bind(this)}>
-          <input type="text" ref={ el => this.inputEl = el }/>
-          <input type="submit"/>
-          <ul>
+        <Card.Group itemsPerRow={6}>
             {
-              this.state.items.map( item => <li key={item.id}>{item.text}</li> )
+              this.state.items.map( item => {
+                return (
+                  <Card key={item.id}>
+                    <Image src='/assets/images/avatar/large/matthew.png' />
+                    <Card.Content>
+                      <Card.Header>
+                        {item.text}
+                      </Card.Header>
+                      <Card.Meta>
+                        <span className='date'>
+                          Joined in 2015
+                        </span>
+                      </Card.Meta>
+                      <Card.Description>
+                        Matthew is a musician living in Nashville.
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <a>
+                        <Icon name='user' />
+                        22 Friends
+                      </a>
+                    </Card.Content>
+                  </Card>
+                )
+              })
             }
-          </ul>
-        </form>
+            <Card>
+              <form onSubmit={this.addItem.bind(this)}>
+                <input type="text" ref={ el => this.inputEl = el }/>
+                <input type="submit"/>
+              </form>
+            </Card>
+          </Card.Group>
       </div>
     );
   }
